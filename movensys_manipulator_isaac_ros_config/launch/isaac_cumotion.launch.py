10f67cef4c26457e690a11c12c0c5189ecca8117
mfikih <mfikih@movensys.com>
2026-07-28 13:38:50 +0900

    add servo node on cumotion

--- movensys_manipulator_isaac_ros_config/launch/isaac_cumotion.launch.py
+++ movensys_manipulator_isaac_ros_config/launch/isaac_cumotion.launch.py
@@ -36,7 +36,8 @@ from launch.conditions import IfCondition
 from launch.launch_context import LaunchContext
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
+from launch_ros.actions import ComposableNodeContainer, Node
+from launch_ros.descriptions import ComposableNode
 from launch_ros.substitutions import FindPackageShare
 from moveit_configs_utils import MoveItConfigsBuilder
 import xacro
@@ -209,12 +210,40 @@ def launch_setup(context: LaunchContext, *args, **kwargs) -> List[Node]:
         launch_arguments={'use_sim_time': use_sim_time}.items(),
     )
 
+    servo_yaml = load_yaml(
+        'movensys_manipulator_moveit_config',
+        os.path.join('config', manipulator_model, 'servo.yaml'),
+    )
+    servo_container = ComposableNodeContainer(
+        name='servo_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container_mt',
+        output='screen',
+        composable_node_descriptions=[
+            ComposableNode(
+                package='moveit_servo',
+                plugin='moveit_servo::ServoNode',
+                name='servo_node',
+                parameters=[
+                    {'moveit_servo': servo_yaml},
+                    moveit_config.robot_description,
+                    moveit_config.robot_description_semantic,
+                    moveit_config.robot_description_kinematics,
+                    moveit_config.joint_limits,
+                    {'use_sim_time': use_sim_time},
+                ],
+            ),
+        ],
+    )
+
     nodes = [
         robot_state_publisher,
         move_group_node,
         cumotion_planner_node,
         rviz_node,
         api_launch,
+        servo_container,
     ]
 
     if static_planning_scene_server is not None:
